d26363b3cfbc74287a67d49a2bc00e599a6d7300
Aloa0001 <alex.oachesu0001@stud.hkr.se>
2019-08-20 13:14:52 +0200

    hash class

--- src/fundationsOfCSDataStructures/ArraysExercises.java
+++ src/fundationsOfCSDataStructures/ArraysExercises.java
@@ -1,24 +1,74 @@
 package fundationsOfCSDataStructures;
 
-import java.util.ArrayList;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class ArraysExercises {
     public static void main(String[] args) {
+        /**insert element in an array*/
         String[] arr = {"one", "two","three"};
-        insertElementInArray(arr,"four",1);
+        System.out.println(insertElementInArray(arr,"four",3));
+        String text = "my name is Alexander";
+        System.out.println(text.charAt(14));
+        int i = text.charAt(0);// the integer will be the unicode value: 'm' is 109 in unicode
+        System.out.println(i);
+
+        /**arrays*/
+        String name = "Alexander";
+        System.out.println(name.toCharArray()[3]);
+
+        Stack<Character> surname = new Stack<>();
+        for(char x: name.toCharArray()){
+            surname.push(x); }
+        System.out.println(surname);
+
+        /**queue*/
+        Queue<Character> queue = new LinkedList<>();
+        queue.addAll(surname);
+        System.out.println(queue.peek());
+        queue.offer('Q');
+        System.out.println(queue);
+        System.out.println(queue.poll());
+        System.out.println(queue.hashCode());
     }
 
     private static <T> ArrayList<T> insertElementInArray(T[] arr, T element, int indexToInsert){
+
         ArrayList<T> newArr = new ArrayList<>();
-        for(int i = 0; i <= arr.length; i ++){
-            if(i > indexToInsert){
-                newArr.set(i+1,arr[i]);
-            }else if(i == indexToInsert){
-                newArr.set(i, element);
-            }else {
-                newArr.set(i , arr[i]);
+
+        try {
+
+            for(int i = 0 ; i < arr.length + 1 ; i++){
+                if(i<indexToInsert){
+                    newArr.add(arr[i]);
+                }else if( i == indexToInsert){
+                    newArr.add(element);
+                }else{
+                    newArr.add(arr[i-1]);
+                }
             }
+
+        }catch (IndexOutOfBoundsException err){
+            System.err.println(err.fillInStackTrace());
         }
         return newArr;
+    }
+
+    private static <T> T[] insertElInArr(T[] arr, T element, int index){
+
+        T[] array = (T[]) new Object[arr.length+1];
+        for (int i = arr.length; i <=0 ; i--){
+            if(i>=index){
+                array[i]=arr[i-1];
+            }else if(  i == index-1){
+                array[i] = element;
+            }else{
+                array[i] = arr [i];
+            }
+
+            }
+
+        return array;
+
     }
 }
---
+++ src/linkedList/Hashes.java
@@ -0,0 +1,40 @@
+package linkedList;
+
+import com.sun.net.httpserver.Filter;
+
+import java.util.Collections;
+import java.util.Hashtable;
+
+public class Hashes {
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        String hash = "Alexander";
+        int i = hashXCode(hash);
+        System.out.println(i);
+        System.out.println(hash.hashCode());
+
+        Hashtable<Integer, String> table = new Hashtable<>();
+        System.out.println(table);
+        System.out.println(table.contains(hash));
+        System.out.println();
+
+    }
+
+    /**hashCode method*/
+    public static int hashXCode(String s){
+        int g = 31;
+        int hash = 0;
+        for(int i = 0 ; i<s.length(); i++){
+            hash = g*hash + s.charAt(i);
+        }
+        return hash;
+    }
+}
