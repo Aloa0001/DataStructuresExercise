8b2d8c3bf0243db294b0737c921211ccf761b3da
Aloa0001 <alex.oachesu0001@stud.hkr.se>
2019-11-19 15:16:21 +0100

    mcCarthy-91

--- src/lectures/recursion/McCarthy_91.java
+++ src/lectures/recursion/McCarthy_91.java
@@ -2,8 +2,9 @@ package lectures.recursion;
 
 public class McCarthy_91 {
     public static void main(String[] args) {
-        System.out.println(mcCarthy_91(103));
-        System.out.println(iterativeF(103));
+        System.out.println(mcCarthy_91(87));
+        System.out.println(iterativeF(87));
+        System.out.println(iterativeMcCarthy_91(87));
     }
 
     private static int mcCarthy_91(int n) {
@@ -13,7 +14,22 @@ public class McCarthy_91 {
             return mcCarthy_91(mcCarthy_91(n + 11));
         }
     }
-
+    private static int iterativeMcCarthy_91(int n) {
+        if (n > 100){
+            return n - 10;
+        }else{
+            while( n <=  100){
+                n = n + 11;
+                if(n > 100){
+                    n = n - 10;
+                }else{
+                    n = n + 1;
+                }
+            }
+            n = n - 10;
+            return n;
+        }
+    }
     private static int iterativeF(int n) {
        if (n > 100){
            return n-10;
@@ -27,4 +43,5 @@ public class McCarthy_91 {
            return n;
        }
     }
+
 }
