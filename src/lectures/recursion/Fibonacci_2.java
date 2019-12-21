9c695ab0d4bccd7630a05fad9fbd45ab68b6dbbb
Aloa0001 <alex.oachesu0001@stud.hkr.se>
2019-12-21 16:15:57 +0100

    fibonacci

--- src/lectures/recursion/Fibonacci_2.java
+++ src/lectures/recursion/Fibonacci_2.java
@@ -2,20 +2,28 @@ package lectures.recursion;
 
 public class Fibonacci_2 {
     public static void main(String[] args) {
-        System.out.println(fibonacci(6));
+        System.out.println(fibonacci(20));
+        System.out.println(fibon_acci(20));
     }
-    private static int fibonacci(int n){
+    private static int fibon_acci(int n){
         if (n<3){
             return 1;
         }
         if(n % 2 == 1){
             int a = (n+1)/2;
             int b = (n-1)/2;
-            return (fibonacci(a)*fibonacci(a) + fibonacci(b)*fibonacci(b));
+            return (fibon_acci(a)*fibon_acci(a) + fibon_acci(b)*fibon_acci(b));
         }else{
             int a = (n/2)+1;
             int b = (n/2)-1;
-            return (fibonacci(a)*fibonacci(a) - fibonacci(b)*fibonacci(b));
+            return (fibon_acci(a)*fibon_acci(a) - fibon_acci(b)*fibon_acci(b));
+        }
+    }
+    private static long fibonacci(int n){
+        if (n <= 1){
+            return n;
+        }else{
+            return fibon_acci(n-1) + fibonacci(n-2);
         }
     }
 }
